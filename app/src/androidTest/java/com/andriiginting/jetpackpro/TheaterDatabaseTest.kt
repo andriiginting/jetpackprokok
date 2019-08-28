30487ffaa5d9137d93403596730ffe3a0017d91f
andriiginting <andri.ginting@go-jek.com>
2019-08-29 00:28:25 +0700

    [andri] update database test to delete data

--- app/src/androidTest/java/com/andriiginting/jetpackpro/TheaterDatabaseTest.kt
+++ app/src/androidTest/java/com/andriiginting/jetpackpro/TheaterDatabaseTest.kt
@@ -42,6 +42,26 @@ open class TheaterDatabaseTest {
         }
     }
 
+    @Test
+    fun deleteTheaterDataToDatabase() {
+        val theater = TheaterFavorite(
+            theaterFavoriteId = "31917",
+            posterPath = "/vC324sdfcS313vh9QXwijLIHPJp.jpg",
+            theaterTitle = "Pretty Little Liars",
+            overview = "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.",
+            backdropPath = "/rQGBjWNveVeF8f2PGRtS85w9o9r.jpg",
+            releaseDate = "2012-10-10"
+        )
+
+        database.theaterDAO().insertFavoriteTheater(theater).test()
+        val delete = database.theaterDAO().deleteTheater("31917").test()
+
+        delete.apply {
+            assertNoErrors()
+            assertComplete()
+        }
+    }
+
     @After
     fun tearDB() = database.close()
 
